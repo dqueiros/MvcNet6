af7032033887abd2bc9ff252894c974260205d0e
unknown <diegoqueiros@hotmail.com>
2023-02-26 07:47:20 -0300

    criando o Modelo de dominio

---
+++ LanchesMac.Web/LanchesMac.Domain/Entities/Categoria.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LanchesMac.Domain.Entities
+{
+    public sealed class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descriçao { get; set; }
+
+        public List<Lanche> Lanches { get; set; }
+    }
+}
