e725062963ebe9185499b84105ae1e5b0c102f85
unknown <diegoqueiros@hotmail.com>
2023-02-26 19:18:34 -0300

    add arquivo de mapeamento

---
+++ LanchesMac.Web/LanchesMac.Infra.Data/Maps/CategoriaMap.cs
@@ -0,0 +1,30 @@
+﻿using LanchesMac.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Infra.Data.Maps
+{
+    public class CategoriaMap : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.ToTable("Categorias");
+
+            builder.HasKey(c => c.CategoriaId);
+
+            builder.Property(c => c.CategoriaId).
+                HasColumnName("CategoriaId")
+                .UseIdentityColumn();
+
+            builder.Property(c => c.CategoriaNome)
+                .HasColumnName("CategoriaNome");
+
+            builder.Property(c => c.Descriçao)
+                .HasColumnName("Descricao");
+
+            builder.HasMany(c => c.Lanches)
+                .WithOne(p => p.Categoria)
+                .HasForeignKey(c => c.CategoriaId);
+        }
+    }
+}
---
+++ LanchesMac.Web/LanchesMac.Infra.Data/Maps/LancheMap.cs
@@ -0,0 +1,52 @@
+﻿
+using LanchesMac.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace LanchesMac.Infra.Data.Maps
+{
+    public class LancheMap : IEntityTypeConfiguration<Lanche>
+    {
+        public void Configure(EntityTypeBuilder<Lanche> builder)
+        {
+            builder.ToTable("Lanches");
+
+            builder.HasKey(c => c.LancheId);
+
+            builder.Property(c => c.LancheId).
+                HasColumnName("LancheId")
+                .UseIdentityColumn();
+
+            builder.Property(c => c.Nome)
+                .HasColumnName("Nome");
+
+            builder.Property(c => c.DescricaoCurta)
+                .HasColumnName("DescricaoCurta");
+
+            builder.Property(c => c.DescricaoDetalhada)
+                .HasColumnName("DescricaoDetalhada");
+
+            builder.Property(c => c.Preco)
+                .HasColumnName("Preco");
+
+            builder.Property(c => c.ImagemUrl)
+                .HasColumnName("ImagemUrl");
+
+            builder.Property(c => c.ImagemThumbnailUrl)
+                .HasColumnName("ImagemThumbnailUrl");
+
+            builder.Property(c => c.IsLanchePreferido)
+                .HasColumnName("IsLanchePreferido");
+
+            builder.Property(c => c.EmEstoque)
+                .HasColumnName("EmEstoque");
+
+            builder.Property(c => c.CategoriaId)
+                .HasColumnName("CategoriaId");
+
+            builder.HasOne(x => x.Categoria)
+                .WithMany(x => x.Lanches);
+        }
+    }
+}
