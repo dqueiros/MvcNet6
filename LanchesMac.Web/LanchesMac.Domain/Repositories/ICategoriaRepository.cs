733ea5f25bca8717c49002fdf51281c5b111769e
unknown <diegoqueiros@hotmail.com>
2023-02-26 16:24:13 -0300

    implementando o padrão repository para categoria

---
+++ LanchesMac.Web/LanchesMac.Domain/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,9 @@
+﻿using LanchesMac.Domain.Entities;
+
+namespace LanchesMac.Domain.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        Task<ICollection<Categoria>> GetCategoriesAsync();
+    }
+}
---
+++ LanchesMac.Web/LanchesMac.Infra.Data/Repositories/CategoriaRepository.cs
@@ -0,0 +1,23 @@
+﻿
+using LanchesMac.Domain.Entities;
+using LanchesMac.Domain.Repositories;
+using LanchesMac.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Infra.Data.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Categoria>> GetCategoriesAsync()
+        {
+            return await _context.Categorias.ToListAsync();
+        }
+    }
+}
