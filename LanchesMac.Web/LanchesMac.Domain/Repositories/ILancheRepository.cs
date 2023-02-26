a0a5d37a20a6ef71f3ef3008696c4302fa40ecc5
unknown <diegoqueiros@hotmail.com>
2023-02-26 16:39:39 -0300

    implementando o repository para lanches

---
+++ LanchesMac.Web/LanchesMac.Domain/Repositories/ILancheRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using LanchesMac.Domain.Entities;
+
+namespace LanchesMac.Domain.Repositories
+{
+    public interface ILancheRepository
+    {
+        Task<ICollection<Lanche>> GetLanchesAsync();
+        Task<ICollection<Lanche>> GetLanchesPreferidosAsync();
+        Task<Lanche> GetByIdAsync(int id);
+    }
+}
---
+++ LanchesMac.Web/LanchesMac.Infra.Data/Repositories/LancheRepository.cs
@@ -0,0 +1,33 @@
+﻿
+using LanchesMac.Domain.Entities;
+using LanchesMac.Domain.Repositories;
+using LanchesMac.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Infra.Data.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LancheRepository (AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Lanche> GetByIdAsync(int id)
+        {
+            return await _context.Lanches.FirstOrDefaultAsync(x => x.LancheId == id);
+        }
+
+        public async Task<ICollection<Lanche>> GetLanchesAsync()
+        {
+            return await _context.Lanches.ToListAsync();
+        }
+
+        public async Task<ICollection<Lanche>> GetLanchesPreferidosAsync()
+        {
+            return await _context.Lanches.Where(p => p.IsLanchePreferido).Include("Categoria").ToListAsync();
+        }
+    }
+}
