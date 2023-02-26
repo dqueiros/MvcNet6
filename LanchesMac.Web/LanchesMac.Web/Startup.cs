973f9c090775f06c8a475036881d5591c593a463
unknown <diegoqueiros@hotmail.com>
2023-02-26 19:20:35 -0300

    chamando o novo servico de injecao

--- LanchesMac.Web/LanchesMac.Web/Startup.cs
+++ LanchesMac.Web/LanchesMac.Web/Startup.cs
@@ -15,7 +15,9 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AdicionaInversaoDeControle();
+        services.AddInfrastructure(Configuration);
+        services.AddService(Configuration);
+
         services.AddControllersWithViews();
     }
 
