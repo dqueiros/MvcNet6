027b7b244d1152e116e6b526b3b05f96a06511e0
unknown <diegoqueiros@hotmail.com>
2023-02-26 09:06:33 -0300

    Configurando o EF Core, context class e connectionString

---
+++ LanchesMac.Web/LanchesMac.Infra.Data/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LanchesMac.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Infra.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        DbSet<Categoria> Categorias { get; set; }
+        DbSet<Lanche> Lanches { get; set; }
+    }
+}
