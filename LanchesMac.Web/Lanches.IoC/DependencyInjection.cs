48030e42374493d17e09fbed7ff65d6ef6756044
unknown <diegoqueiros@hotmail.com>
2023-02-26 19:17:41 -0300

    removido arquivos e criado um novo arquivo de injecao

---
+++ LanchesMac.Web/Lanches.IoC/DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿
+using Lanches.Application.Services.Interfaces;
+using Lanches.Application.Services;
+using LanchesMac.Domain.Repositories;
+using LanchesMac.Infra.Data.Context;
+using LanchesMac.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lanches.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+            services.AddScoped<ILancheRepository, LancheRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICategoriaService, CategoriaService>();
+            services.AddScoped<ILancheService, LancheService>();
+            
+            return services;
+        }
+    }
+}
