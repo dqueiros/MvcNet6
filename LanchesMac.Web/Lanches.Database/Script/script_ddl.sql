c66dcf35f54df0eb06d86be6781e41a4935e997b
unknown <diegoqueiros@hotmail.com>
2023-02-26 15:24:06 -0300

    script ddl

---
+++ LanchesMac.Web/Lanches.Database/Script/script_ddl.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE [LanchesDataBase]
+
+USE LanchesDataBase
+
+CREATE TABLE [dbo].[Categorias](
+	[CategoriaId] [int] PRIMARY KEY IDENTITY NOT NULL,
+	[CategoriaNome] [nvarchar](100) NOT NULL,
+	[Descricao] [nvarchar](200) NOT NULL,
+);
+
+CREATE TABLE [dbo].[Lanches](
+	[LancheId] [int] PRIMARY KEY IDENTITY NOT NULL,
+	[Nome] [nvarchar](80) NOT NULL,
+	[DescricaoCurta] [nvarchar](200) NOT NULL,
+	[DescricaoDetalhada] [nvarchar](200) NOT NULL,
+	[Preco] [decimal](10,2) NOT NULL,
+	[ImageUrl] [nvarchar](200) NULL,
+	[ImageTumbnailUrl] [nvarchar](200) NULL,
+	[IsLanchePreferido] [bit] NOT NULL,
+	[EmEstoque] [bit] NOT NULL,
+	[CategoriaId] [int] NOT NULL,
+	CONSTRAINT FK_Categoria_Lanche FOREIGN KEY (CategoriaId) REFERENCES [dbo].[Categorias](CategoriaId)
+);
