b495dcd352d015f70e2b4e06a1a3f81662855f80
unknown <diegoqueiros@hotmail.com>
2023-03-04 07:46:53 -0300

    metodos no projeto application

--- LanchesMac.Web/Lanches.Application/Services/Interfaces/ILancheService.cs
+++ LanchesMac.Web/Lanches.Application/Services/Interfaces/ILancheService.cs
@@ -1,7 +1,10 @@
 ﻿
+using LanchesMac.Domain.Entities;
+
 namespace Lanches.Application.Services.Interfaces
 {
     public interface ILancheService
     {
+        Task<ICollection<Lanche>> GetLanchesAsync();
     }
 }
--- LanchesMac.Web/Lanches.Application/Services/LancheService.cs
+++ LanchesMac.Web/Lanches.Application/Services/LancheService.cs
@@ -1,8 +1,21 @@
 ﻿using Lanches.Application.Services.Interfaces;
+using LanchesMac.Domain.Entities;
+using LanchesMac.Domain.Repositories;
 
 namespace Lanches.Application.Services
 {
     public class LancheService : ILancheService
     {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheService(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public async Task<ICollection<Lanche>> GetLanchesAsync()
+        {
+            return await _lancheRepository.GetLanchesAsync();
+        }
     }
 }
