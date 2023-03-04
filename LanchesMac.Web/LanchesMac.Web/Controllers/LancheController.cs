ebc55319b8954a04bf310a68a0bacfc1d1d8b011
unknown <diegoqueiros@hotmail.com>
2023-03-04 07:49:41 -0300

    criando o Controller a View para exibir os lanches

---
+++ LanchesMac.Web/LanchesMac.Web/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using Lanches.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Web.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheService _lancheService;
+
+        public LancheController(ILancheService lancheService)
+        {
+            _lancheService = lancheService;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var lanches = await _lancheService.GetLanchesAsync();
+            return View(lanches);
+        }
+    }
+}
