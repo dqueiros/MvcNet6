8b96e23e8d41ba679f03be05cb066c748b5325ad
unknown <diegoqueiros@hotmail.com>
2023-03-12 15:47:39 -0300

    criando uma TagHelper

---
+++ LanchesMac.Web/LanchesMac.Web/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Web.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
