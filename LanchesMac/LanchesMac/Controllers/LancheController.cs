4e18a8f432af14842e40bf29ebefa31440fb6e00
unknown <doqueiros@apoioprodesp.sp.gov.br>
2023-05-21 18:03:02 -0300

    Implementando a view model LancheListViewModel

--- LanchesMac/LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -14,8 +15,15 @@ namespace LanchesMac.Controllers
 
         public IActionResult List()
         {
-            var lanches = _lancheRepository.Lanches;
-            return View(lanches);
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lanchesListViewModel);
+
         }
     }
 }
