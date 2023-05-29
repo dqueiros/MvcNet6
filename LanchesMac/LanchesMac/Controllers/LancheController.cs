88a864a9f08da7968c41739e4b84d789515f10d7
unknown <doqueiros@apoioprodesp.sp.gov.br>
2023-05-29 20:09:30 -0300

    otimizando o método Action List de LancheController

--- LanchesMac/LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/LanchesMac/Controllers/LancheController.cs
@@ -26,18 +26,10 @@ namespace LanchesMac.Controllers
             }
             else
             {
-                if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
-                {
-                    lanches = _lancheRepository.Lanches
-                        .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
-                        .OrderBy(l => l.Nome);
-                }
-                else
-                {
-                    lanches = _lancheRepository.Lanches
-                            .Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
+                lanches = _lancheRepository.Lanches
+                            .Where(l => l.Categoria.CategoriaNome.Equals(categoria))
                             .OrderBy(l => l.Nome);
-                }
+
                 categoriaAtual = categoria;
             }
 
