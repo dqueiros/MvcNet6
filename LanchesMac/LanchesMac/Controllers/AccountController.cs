54fcd17f0cef81a51fa2f7af65b76d9a5091b3de
unknown <doqueiros@apoioprodesp.sp.gov.br>
2023-06-12 20:13:24 -0300

    implementando o Registro

--- LanchesMac/LanchesMac/Controllers/AccountController.cs
+++ LanchesMac/LanchesMac/Controllers/AccountController.cs
@@ -46,5 +46,32 @@ namespace LanchesMac.Controllers
             ModelState.AddModelError("", "Falha ao realizar o login!!");
             return View(loginVM);
         }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginViewModel registroVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = registroVM.UserName };
+                var result = await _userManager.CreateAsync(user, registroVM.Password);
+
+                if (result.Succeeded)
+                {
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    this.ModelState.AddModelError("Registro", "Falha ao registrar o usuário");
+                }
+            }
+            return View(registroVM);
+        }
     }
 }
