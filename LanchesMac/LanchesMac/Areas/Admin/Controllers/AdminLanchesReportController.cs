21b40a5a649af1a4015f201df71cada1460784b6
unknown <doqueiros@apoioprodesp.sp.gov.br>
2024-06-17 20:50:33 -0300

    criando o Controller e a View para gerar o relatorio

---
+++ LanchesMac/LanchesMac/Areas/Admin/Controllers/AdminLanchesReportController.cs
@@ -0,0 +1,40 @@
+﻿using FastReport.Data;
+using FastReport.Export.PdfSimple;
+using FastReport.Web;
+using LanchesMac.Areas.Admin.FastReportUtils;
+using LanchesMac.Areas.Admin.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class AdminLanchesReportController : Controller
+{
+    private readonly IWebHostEnvironment _webHostEnv;
+    private readonly RelatorioLanchesService _relatorioLanchesService;
+
+    public AdminLanchesReportController(IWebHostEnvironment webHostEnv,
+        RelatorioLanchesService relatorioLanchesService)
+    {
+        _webHostEnv = webHostEnv;
+        _relatorioLanchesService = relatorioLanchesService;
+    }
+    [Route("LanchesCategoriaReport")]
+    public async Task<ActionResult> LanchesCategoriaReport()
+    {
+        var webReport = new WebReport();
+        var mssqlDataConnection = new MsSqlDataConnection();
+
+        webReport.Report.Dictionary.AddChild(mssqlDataConnection);
+
+        webReport.Report.Load(Path.Combine(_webHostEnv.ContentRootPath, "wwwroot/reports",
+                                           "lanchesCategoria.frx"));
+
+        var lanches = HelperFastReport.GetTable(await _relatorioLanchesService.GetLanchesReport(), "LanchesReport");
+        var categorias = HelperFastReport.GetTable(await _relatorioLanchesService.GetCategoriasReport(), "CategoriasReport");
+
+        webReport.Report.RegisterData(lanches, "LancheReport");
+        webReport.Report.RegisterData(categorias, "CategoriasReport");
+        return View(webReport);
+    }
+}
