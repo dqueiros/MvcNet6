028a6905dd4bb1363b9ed8865919c0d8aa0c3143
unknown <doqueiros@apoioprodesp.sp.gov.br>
2024-06-17 20:29:55 -0300

    criando os servicos para obter os dados

--- LanchesMac/LanchesMac/Program.cs
+++ LanchesMac/LanchesMac/Program.cs
@@ -56,6 +56,7 @@ builder.Services.AddTransient<IPedidoRepository, PedidoRepository>();
 builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
 builder.Services.AddScoped<RelatorioVendasService>();
 builder.Services.AddScoped<GraficoVendasService>();
+builder.Services.AddScoped<RelatorioLanchesService>();
 
 builder.Services.AddAuthorization(options =>
 {
